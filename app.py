8caba308fe0f53abd2ca01bf9519586076bbc26a
ckmonish2000 <ckmonish2000@gmail.com>
2020-03-23 16:50:09 +0530

    third commit

--- app.py
+++ app.py
@@ -1,6 +1,38 @@
-from flask import Flask,render_template,request
+from flask import Flask,jsonify,request
 from scraper import scrape
 
+app=Flask(__name__)
 
-x=scrape()
-print(x)
+@app.route("/")
+def index():
+    return "Welcome To Covid-19-API-Generator"
+
+
+@app.route('/all')
+def total():
+    a=scrape()   
+    return jsonify({"head":"from covid-19-generator","status":200,"body":a})
+
+
+@app.route("/country/<string:ctr>")
+def test(ctr):
+    val=[]
+    a=ctr.capitalize()
+    print(a)
+    b=scrape()
+    for x in b:
+        if(x['country']==a):
+            val.append(x)
+            break
+        else:
+            pass
+    return jsonify({"head":"from covid-19-generator","status":200,"body":val})
+
+        
+
+    return "works"
+
+
+
+if __name__=="__main__":
+    app.run()
--- scraper.py
+++ scraper.py
@@ -10,9 +10,7 @@ def scrape():
 
     # print(soup)
 
-    b=soup.find(class_='maincounter-number')
-
-    print(b)
+    
 
     c=soup.select('tbody')
     for x in c:
@@ -20,7 +18,6 @@ def scrape():
         y=x.select('tr')
         for td in y:
             v=td.findAll('td')
-            print(v[0].get_text())
             lst.append({'country':str(v[0].get_text()),'TotalCases':str(v[1].get_text()),'NewCases':str(v[2].get_text()),'TotalDeath':str(v[3].get_text()),'NewDeath':str(v[4].get_text()),'TotalRecovery':str(v[5].get_text()),'ActiveCases':str(v[6].get_text()),'Critical':str(v[7].get_text())})
     return lst
 
