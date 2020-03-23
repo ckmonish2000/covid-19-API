03252b3e713643f4fdfda65893be87b931d69daa
ckmonish2000 <ckmonish2000@gmail.com>
2020-03-23 16:01:01 +0530

    second commit

--- app.py
+++ app.py
@@ -1,26 +1,6 @@
 from flask import Flask,render_template,request
-import requests
-from bs4 import BeautifulSoup
+from scraper import scrape
 
 
-
-
-a=requests.get("https://www.worldometers.info/coronavirus/")
-print(a)
-soup=BeautifulSoup(a.text,'html.parser')
-
-# print(soup)
-
-b=soup.find(class_='maincounter-number')
-
-print(b)
-
-c=soup.select('tbody')
-for x in c:
-    # print(x)
-    y=x.select('tr')
-    for td in y:
-        v=td.findAll('td')
-        print(v[0].get_text())
-    
-    
+x=scrape()
+print(x)
